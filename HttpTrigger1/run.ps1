6e9bcc9cc587b2b7e25e64d709d982306e638e27
James Godden <jgodden90@outlook.com>
2023-03-28 23:47:54 -0400

    added call to start vm

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -18,6 +18,8 @@ if ($name) {
     $body = "Hello, $name. This HTTP triggered function executed successfully."
 }
 
+Start-AzVM -ResourceGroupName "Main" -Name "Valheim"
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
